1b26ad61b97afe8a75c3e2fd624c4ba17e332a7e
SrivaniMalladi <srivanigorti@gmail.com>
2018-04-21 18:28:41 +0530

    added one method

--- voicerecorder/src/main/java/com/example/voicerecorder/VoiceActivity.java
+++ voicerecorder/src/main/java/com/example/voicerecorder/VoiceActivity.java
@@ -1,13 +1,19 @@
 package com.example.voicerecorder;
 
 import android.app.Application;
+import android.util.Log;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
 public class VoiceActivity  {
 
-    private ImageButton voiceButton;
-    private EditText editText;
+    private static final String TAG =" message";
+
+    public void show(){
+        Log.d(TAG,"hello");
+    }
+
+
 
 
 
